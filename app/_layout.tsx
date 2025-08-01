6ffdfed03f46d11a4af80171df646ca45acee1a0
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2025-08-01 22:42:13 +0300

    Enhance input interface in RootLayout by adding styling and structure. Introduced inputContainer style for better layout and added textInput style for improved user experience.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function RootLayout() {
   return (
     <View style={styles.appContainer}>
-      <View>
+      <View style={styles.inputContainer}>
         <TextInput placeholder="Your course goal!" />
         <Button title="Add Goal" />
       </View>
@@ -19,8 +19,22 @@ const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
   },
+
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
 });
 /**
  * 1. Create a new project => npx create-expo-app@latest
+ * Proje Başlangıç : npx expo start
  * <View> komponenti => React Native'in birinci komponenti. Bununla birlikte <Text> komponenti de kullanılır.
  */
